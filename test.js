f95f5650bf7318aa81cdbcd1d02f804eb87255cb
Your Name <iamtekeste@gmail.com>
2016-02-20 23:49:28 -0600

    fix test.js

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 import test from 'ava'
 import iconThemePath from './index.js'
 test(t => {
-    t.plan(2)
     return iconThemePath().then(result => {
-        console.log(result)
         t.is(typeof result, 'string')
-        t.true('icons' in result)
+        t.true(result.indexOf('/usr/share/icons/') > -1 )
     });
 });
